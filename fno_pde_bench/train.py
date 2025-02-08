41e1d65d067d24966fa9ec8051730735bb94c817
wyichen <23110840012@m.fudan.edu.cn>
2025-02-08 18:25:58 +0800

    Add fno with pdebench and origin data

--- fno_pde_bench/train.py
+++ fno_pde_bench/train.py
@@ -210,6 +210,50 @@ def test_loop(dataloader, model, device, initial_step, metric_names=['MSE', 'RMS
         res_dict[name] = res
     return res_dict
 
+class LpLoss(object):
+    def __init__(self, d=2, p=2, size_average=True, reduction=True):
+        super(LpLoss, self).__init__()
+
+        #Dimension and Lp-norm type are postive
+        assert d > 0 and p > 0
+
+        self.d = d
+        self.p = p
+        self.reduction = reduction
+        self.size_average = size_average
+
+    def abs(self, x, y):
+        num_examples = x.size()[0]
+
+        #Assume uniform mesh
+        h = 1.0 / (x.size()[1] - 1.0)
+
+        all_norms = (h**(self.d/self.p))*torch.norm(x.view(num_examples,-1) - y.view(num_examples,-1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return torch.mean(all_norms)
+            else:
+                return torch.sum(all_norms)
+
+        return all_norms
+
+    def rel(self, x, y):
+        num_examples = x.size()[0]
+
+        diff_norms = torch.norm(x.reshape(num_examples,-1) - y.reshape(num_examples,-1), self.p, 1)
+        y_norms = torch.norm(y.reshape(num_examples,-1), self.p, 1)
+
+        if self.reduction:
+            if self.size_average:
+                return torch.mean(diff_norms/y_norms)
+            else:
+                return torch.sum(diff_norms/y_norms)
+
+        return diff_norms/y_norms
+
+    def __call__(self, x, y):
+        return self.rel(x, y)
 
 def main(args):
     #init
@@ -283,8 +327,8 @@ def main(args):
     scheduler = getattr(torch.optim.lr_scheduler, sched_name)(optimizer, last_epoch=start_epoch-1, **sched_args)
 
     # loss function
-    loss_fn = nn.MSELoss(reduction="mean")
-
+    #loss_fn = nn.MSELoss(reduction="mean")
+    loss_fn=LpLoss(size_average=False)
     # save loss history
     loss_history = []
     if args["continue_training"]:
@@ -327,8 +371,8 @@ def main(args):
 
 
 if __name__ == "__main__":
-    config_file='./config/config_2D_CFD.yaml'
-    #config_file='./config/config_2D_Darcy_Flow.yaml'
+    #config_file='./config/config_2D_CFD.yaml'
+    config_file='./config/config_2D_Darcy_Flow.yaml'
     #config_file='./config/config_1D_Burgers.yaml'
     with open(config_file, 'r') as f:
         args = yaml.safe_load(f)
